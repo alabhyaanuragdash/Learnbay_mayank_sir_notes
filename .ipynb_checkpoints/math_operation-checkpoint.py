c7790d3178f85afa97258dd4c0acadd13b05130d
Alabhya Anurag Dash <anuragdash2104@gmail.com>
2024-10-04 19:36:13 +0530

    Flies are updated

---
+++ .ipynb_checkpoints/math_operation-checkpoint.py
@@ -0,0 +1,47 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
+def power(x,y):
+    return x**y
+
+def floor_div(x,y):
+    return x//y
+
+def average(x,y):
+    return (x+y)/2
+    
+def factorial(x):
+    if x==1 or x==0:
+        return 1
+    elif x<0:
+        print("enter valid no")
+    else:
+        return (x*factorial(x-1))
+def max(x,y):
+    if x>y:
+        return x
+    else:
+        return y
+
+def min(x,y):
+    if x<y:
+        return x
+    else:
+        return y
+
+
+
+def cumulative_sum():
+    sum=0
+    for i in n:
+        s=s+i
+    return s
